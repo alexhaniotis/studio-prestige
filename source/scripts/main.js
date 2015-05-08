8e7caa8e7ab4f7c94c172f7335fb4c03fca99f36
Alexander Haniotis <alexanderhaniotis@gmail.com>
2015-05-08 19:08:40 -0400

    Styles, etc.

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,6 +1,20 @@
 //= require modernizr/modernizr
 //= require jquery
+//= require wow
 
 $(document).ready(function() {
+
+  var wow = new WOW(
+    {
+      boxClass:     'wow',      // animated element css class (default is wow)
+      animateClass: 'animated', // animation css class (default is animated)
+      mobile:       false,       // trigger animations on mobile devices (default is true)
+      callback:     function(box) {
+        // the callback is fired every time an animation is started
+        // the argument that is passed in is the DOM node being animated
+      }
+    }
+  );
+  wow.init();
 
 });
