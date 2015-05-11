bdf2272f5a0a5d1c8d97c7d4640dfb137367e442
Alexander Haniotis <alexanderhaniotis@gmail.com>
2015-05-10 21:09:48 -0400

    Animation fixes

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -9,13 +9,9 @@ $(document).ready(function() {
 
   var wow = new WOW(
     {
-      boxClass:     'wow',      // animated element css class (default is wow)
-      animateClass: 'animated', // animation css class (default is animated)
-      mobile:       false,       // trigger animations on mobile devices (default is true)
-      callback:     function(box) {
-        // the callback is fired every time an animation is started
-        // the argument that is passed in is the DOM node being animated
-      }
+      boxClass: 'wow',
+      animateClass: 'animated',
+      mobile: false,
     }
   );
   wow.init();
